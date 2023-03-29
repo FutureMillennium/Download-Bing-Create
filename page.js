748650ba392b8e5694c0212b9dcc67304a82866a
Zdeněk Gromnica <futuremillennium@gmail.com>
2023-03-29 18:20:58 +0200

    add hash when prompt is too long for Windows filenames

--- page.js
+++ page.js
@@ -1,4 +1,14 @@
-(function() {
+(async function() {
+	async function DigestMessage(message) {
+		const msgUint8 = new TextEncoder().encode(message); // encode as (utf-8) Uint8Array
+		const hashBuffer = await crypto.subtle.digest("SHA-1", msgUint8); // hash the message
+		const hashArray = Array.from(new Uint8Array(hashBuffer)); // convert buffer to byte array
+		const hashHex = hashArray
+			.map((b) => b.toString(16).padStart(2, "0"))
+			.join(""); // convert bytes to hex string
+		return hashHex;
+	}
+
 	let imgs = document.querySelectorAll('.img_cont img');
 	if (imgs.length === 0) {
 		imgs = document.querySelectorAll('.giric img');
@@ -10,6 +20,9 @@
 			url: url,
 			filename: prompt + '-' + i + '.jpg'
 		};
+		if (prompt.length > 220) {
+			obj.filename = await DigestMessage(prompt) + ' ' + prompt + '-' + i + '.jpg'
+		}
 		chrome.runtime.sendMessage(obj);
 	}
 })();
