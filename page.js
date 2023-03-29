f815cc987968be740fe7cd59ea6ab69184207ad2
Zdeněk Gromnica <futuremillennium@gmail.com>
2023-03-29 16:00:41 +0200

    fix for only 1 image generated

--- page.js
+++ page.js
@@ -1,5 +1,8 @@
 (function() {
 	let imgs = document.querySelectorAll('.img_cont img');
+	if (imgs.length === 0) {
+		imgs = document.querySelectorAll('.giric img');
+	}
 	let prompt = document.querySelector('#sb_form_q').value;
 	for (let i = 0; i < imgs.length; i++) {
 		let url = imgs[i].src.replace('w=270&h=270&c=6&r=0&o=5&', '');
